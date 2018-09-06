2465540f79b6f178daa0a526ab2035fe60304181
iam91 <zhengwy91@foxmail.com>
2018-09-06 23:11:51 +0800

    update

---
+++ src/tag/string/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,30 @@
+package tag.string;
+
+public class LongestSubstringWithoutRepeating {
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null|| s.length() == 0) return 0;
+        int n = s.length();
+
+        int[] len = new int[n];
+        int[] dict = new int[128];
+
+        for(int i = 0; i < 128; i++) dict[i] = -1;
+        len[0] = 1;
+        dict[s.charAt(0) - '\0'] = 0;
+
+        int max = 1;
+        for(int i = 1; i < n; i++) {
+            char ch = s.charAt(i);
+            if(len[i - 1] >= i - dict[ch - '\0']) {
+                len[i] = i - dict[ch - '\0'];
+            } else {
+                len[i] = len[i - 1] + 1;
+            }
+            dict[ch - '\0'] = i;
+            max = Math.max(max, len[i]);
+        }
+
+        return max;
+    }
+}
