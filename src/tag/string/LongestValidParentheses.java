e40e43bb3eb2e4c3146b6917ec72e8926d29002a
iam91 <zhengwy91@foxmail.com>
2018-09-10 18:06:10 +0800

    update

--- src/tag/string/LongestValidParentheses.java
+++ src/tag/string/LongestValidParentheses.java
@@ -1,38 +1,35 @@
-class Solution {
+package tag.string;
+
+import java.util.Stack;
+
+class LongestValidParentheses {
+    // https://leetcode.com/problems/longest-valid-parentheses/description/
     public int longestValidParentheses(String s) {
         if(s == null || s.length() == 0) return 0;
-        int n = s.length();
 
         Stack<Integer> stack = new Stack<>();
-        int idx = 0;
+        stack.push(-1);
         int max = 0;
-        while(idx < n) {
-            int len = 0, i;
-            // probe
-            for(i = idx; i < n; i++) {
-                char ch = s.charAt(i);
-                if(ch == '(') {
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            }
+            if(s.charAt(i) == ')') {
+                stack.pop();
+                if (stack.empty()) {
                     stack.push(i);
-                    if(stack.size() * 2 + len > n - idx) {
-                        // too many left parentheses
-                        if(stack.size() == 1) {
-
-                        }
-                    }
-                }
-                if(ch == ')') {
-                    if(!stack.isEmpty()) {
-                        stack.pop();
-                        len += 2;
-                    } else {
-                        // too many right parentheses
-                        max = Math.max(max, len);
-                        idx = i + 1;
-                        break;
-                    }
+                } else {
+                    max = Math.max(max, i - stack.peek());
                 }
             }
         }
+
         return max;
+    }
+
+    public static void main(String[] args) {
+        LongestValidParentheses c = new LongestValidParentheses();
+        int len = c.longestValidParentheses(")()())");
+        System.out.println(len);
     }
 }
