fa5667ba1c158b010a304891bfee433f23099605
iam91 <zhengwy91@foxmail.com>
2018-09-11 15:29:46 +0800

    update

--- src/tag/search/SearchMatrixII.java
+++ src/tag/search/SearchMatrixII.java
@@ -3,24 +3,38 @@ package tag.search;
 public class SearchMatrixII {
     // https://leetcode.com/problems/search-a-2d-matrix-ii/description/
 
-    private int m;
-    private int n;
-
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
 
-        this.m = matrix.length;
-        this.n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
+        int x = n - 1, y = 0;
+        while(x >= 0 && y < m) {
+            if(matrix[y][x] == target) return true;
+            else if(matrix[y][x] > target) {
+                x--;
+            } else {
+                y++;
+            }
+        }
 
         return false;
     }
 
-    private int x(int idx) {
-        return idx / this.n;
-    }
+    public static void main(String[] args) {
+        SearchMatrixII c = new SearchMatrixII();
 
-    private int y(int idx) {
-        return idx % this.n;
+//        int[][] matrix = {
+//                {1,4,7,11,15},
+//                {2,5,8,12,19},
+//                {3,6,9,16,22},
+//                {10,13,14,17,24},
+//                {18,21,23,26,30}};
+//        int target = 20;
+        int[][] matrix = {{1, 1}};
+        int target = 0;
+
+        System.out.println(c.searchMatrix(matrix, target));
     }
 }
