eb54482ca8d76cf09dae9da086cdf96a3efcfd0c
iam91 <zhengwy91@foxmail.com>
2018-09-13 13:39:57 +0800

    update

--- src/tag/JumpGame.java
+++ src/tag/JumpGame.java
@@ -3,31 +3,37 @@ package tag;
 public class JumpGame {
     // https://leetcode.com/problems/jump-game/description/
     public boolean canJump(int[] nums) {
-        return naive(nums);
+        // return bruteForce(nums);
+        return greedy(nums);
     }
 
-    private boolean naive(int[] nums) {
+    private boolean bruteForce(int[] nums) {
         if(nums == null || nums.length == 0) return false;
 
         int n = nums.length;
-        int[] dp = new int[n];
+        int[] d = new int[n];
 
-        dp[0] = 1;
+        d[0] = 1;
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
                 if(nums[j] >= i - j) {
-                    dp[i] += dp[j];
+                    d[i] += d[j];
                 }
             }
         }
 
-        return dp[n - 1] > 0;
+        return d[n - 1] > 0;
     }
 
-    private boolean linear(int[] nums) {
+    private boolean greedy(int[] nums) {
         if(nums == null || nums.length == 0) return false;
 
         int n = nums.length;
-
+        int remain = 1;
+        for(int i = 0; i < n; i++) {
+            if(--remain < 0) return false;
+            else remain = Math.max(remain, nums[i]);
+        }
+        return remain >= 0;
     }
 }
